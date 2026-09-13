ea6a3e9e77640059ccdf89834e2edfe1bbbc8d3a
Wiktor <wirekw545@gmail.com>
2026-09-13 13:47:32 +0200

    connect MainViewModel bindings and render initial UI

--- MoveYaFiles/ViewModels/MainViewModel.cs
+++ MoveYaFiles/ViewModels/MainViewModel.cs
@@ -1,8 +1,45 @@
-using CommunityToolkit.Mvvm.ComponentModel;
+using MoveYaFiles.Services;
 
 namespace MoveYaFiles.ViewModels;
 
-public partial class MainViewModel : ViewModelBase
+public class MainViewModel : ViewModelBase
 {
-    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+    private readonly TransferEngine _engine = new();
+    private string _statusMessage = "Ready to use";
+    private string _lastRunText = "No data";
+
+    public string StatusMessage
+    {
+        get => _statusMessage;
+        set => SetProperty(ref _statusMessage, value);
+    }
+
+    public string LastRunText
+    {
+        get => _lastRunText;
+        set => SetProperty(ref _lastRunText, value);
+    }
+
+    public MainViewModel()
+    {
+        RefreshLastRun();
+    }
+
+    public void RunTransfer()
+    {
+        StatusMessage = "Transferring...";
+        
+        var config = _engine.LoadConfig();
+        _engine.ExecuteTransfer(config);
+
+        RefreshLastRun();
+        StatusMessage = "Transfer Complete!";
+    }
+
+    private void RefreshLastRun()
+    {
+        var config = _engine.LoadConfig();
+        LastRunText = config.LastRun.HasValue ? config.LastRun.Value.ToString("yyyy-MM-dd HH:mm:ss") : "Never";
+    }
 }
+
