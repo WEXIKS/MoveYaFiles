d15c4e649d59f9703846f10066da650097f5db86
Wiktor <wirekw545@gmail.com>
2026-09-13 12:32:56 +0200

    AppConfig.cs and config.json has been created and modified

---
+++ MoveYaFiles/Models/AppConfig.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace  MoveYaFiles.Models;
+
+public class TransferRule
+{
+    public string SourcePath { get; set; } = "";
+    public string DestinationPath { get; set; } = "";
+    public string[] AllowedExtensions { get; set; } = Array.Empty<string>();
+    public long MinFileSizeBytes { get; set; } = 0; //2 GB = 2147483648 bajtów
+    public long MaxFileSizeBytes { get; set; } = long.MaxValue;
+    public string ConflictStrategy { get; set; } = "AddTimestamp";
+}
+
+public class AppConfig
+{
+    public int IntervalMinutes { get; set; } = 30;
+    public bool AutoStartWithSystem { get; set; } = false;
+    public DateTime? LastRun { get; set; }
+    public List<TransferRule> Rules { get; set; } = new();
+}
+
---
+++ MoveYaFiles/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MoveYaFiles.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
